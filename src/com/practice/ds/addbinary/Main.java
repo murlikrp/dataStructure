7e7be3e5368529a3e45ab4ad230998a986dba9f2
murlikrp <31443425+murlikrp@users.noreply.github.com>
2023-07-17 21:49:32 -0700

    add binary impl

---
+++ src/com/practice/ds/addbinary/Main.java
@@ -0,0 +1,41 @@
+package com.practice.ds.addbinary;
+
+public class Main {
+	
+	public static String solve(String a, String b) {
+		StringBuilder result = new StringBuilder();
+		int i = a.length() - 1;
+		int j = b.length() - 1;
+		int carry = 0;
+		
+		while(i >= 0 || j >= 0) {
+			int sum = carry;
+			if(i >= 0) {
+				sum += a.charAt(i) - '0';
+				i--;
+			}
+			if(j >= 0) {
+				sum += b.charAt(j) - '0';
+				j--;
+			}
+			
+			result.append(sum % 2);
+			carry = sum/2;
+		}
+		
+		if(carry != 0) {
+			result.append(carry);
+		}
+		return result.reverse().toString();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String a = "101";
+		String b = "1101";
+		String result = solve(a, b);
+		System.out.println(result);
+	}
+
+}
